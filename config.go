227bff713cd28dea12f793ed69af77d33d21bcb3
vasili-v <vasili_vasilyeu@epam.com>
2019-07-29 16:47:37 +0300

    Add search by import

--- config.go
+++ config.go
@@ -1,8 +1,19 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+	"regexp"
+)
+
+const (
+	modeList = iota
+	modeImport
+)
 
 type config struct {
+	mode     int
+	regex    *regexp.Regexp
 	dir      string
 	exclDirs strSet
 }
@@ -12,14 +23,33 @@ func makeConfig() config {
 		exclDirs: make(strSet),
 	}
 
-	flag.StringVar(&c.dir, "dir", "", "directory to search in (default - current directory)")
-	flag.Var(c.exclDirs, "exclude-dir", "name of subdirectory to exclude from search")
+	imp := flag.String("import", "",
+		"search for import matching the regular expression")
+	flag.StringVar(&c.dir, "dir", "",
+		"directory to search in (default - current directory)")
+	flag.Var(c.exclDirs, "exclude-dir",
+		"name of subdirectory to exclude from search")
 
 	flag.Parse()
 
+	c.setMode(*imp)
 	c.validateDir()
 
 	return c
+}
+
+func (c *config) setMode(imp string) {
+	c.mode = modeList
+
+	if imp != "" {
+		r, err := regexp.Compile(imp)
+		if err != nil {
+			log.Fatalf("invalid regular expression for import: %s", err)
+		}
+
+		c.mode = modeImport
+		c.regex = r
+	}
 }
 
 func (c *config) validateDir() {
--- walk.go
+++ walk.go
@@ -11,22 +11,24 @@ import (
 const execPackageName = "main"
 
 func walk(conf config) {
-	if err := filepath.Walk(conf.dir, func(path string, info os.FileInfo, err error) error {
-		if info != nil && info.IsDir() {
-			if _, ok := conf.exclDirs[info.Name()]; ok {
-				return filepath.SkipDir
+	if err := filepath.Walk(conf.dir,
+		func(path string, info os.FileInfo, err error) error {
+			if info != nil && info.IsDir() {
+				if _, ok := conf.exclDirs[info.Name()]; ok {
+					return filepath.SkipDir
+				}
+
+				processDir(path, conf)
 			}
 
-			processDir(conf.dir, path)
-		}
-
-		return nil
-	}); err != nil {
+			return nil
+		},
+	); err != nil {
 		log.Fatalf("failed while walking the path %q: %s", conf.dir, err)
 	}
 }
 
-func processDir(root, path string) {
+func processDir(path string, conf config) {
 	p, err := build.ImportDir(path, 0)
 	if err != nil {
 		if _, ok := err.(*build.NoGoError); !ok {
@@ -36,10 +38,33 @@ func processDir(root, path string) {
 		return
 	}
 
-	fmt.Printf("%s: %s", trimPath(path, root), p.Name)
-	if p.Name == execPackageName {
+	switch conf.mode {
+	case modeList:
+		printPkg(path, p, conf.dir)
+
+	case modeImport:
+		imports := []string{}
+		for _, s := range p.Imports {
+			if conf.regex.MatchString(s) {
+				imports = append(imports, s)
+			}
+		}
+
+		if len(imports) > 0 {
+			printPkg(path, p, conf.dir)
+			for _, s := range imports {
+				fmt.Printf("\t%s\n", s)
+			}
+			fmt.Println()
+		}
+	}
+}
+
+func printPkg(path string, pkg *build.Package, root string) {
+	fmt.Printf("%s: %s", trimPath(path, root), pkg.Name)
+	if pkg.Name == execPackageName {
 		fmt.Printf("\n")
 	} else {
-		fmt.Printf(" (%s)\n", p.ImportPath)
+		fmt.Printf(" (%s)\n", pkg.ImportPath)
 	}
 }
